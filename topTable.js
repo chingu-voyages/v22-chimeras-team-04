9d86e57f6556103c2acccc6a9b39c38ecf295ae8
yanpelov <11660378+yanpelov@users.noreply.github.com>
2020-08-19 23:41:12 +0300

    Added sorting asc/desc for nums and strings

--- topTable.js
+++ topTable.js
@@ -1,27 +1,120 @@
 var table = document.getElementById("topSendersTbl");
 
-var rowCnt = 1;
 var cellCnt = 0;
 
 var action = 'Delete: <a href=_blank>All</a> | <a href=_blank>Selective</a>'
-var data = {row1: {emailContact:"test1@yan.com", emailCnt: 456, Action:action}, row2:{emailContact:"av1@rr.com", emailCnt: 3333, Action:action }};
+var data =  [{emailContact:"test1@yan.com", emailCnt: 456, Action:action}, 
+            {emailContact:"av1@rr.com", emailCnt: 3333, Action:action },
+            {emailContact:"bv1@rr.com", emailCnt: 2, Action:action },
+            {emailContact:"twest1@gg.com", emailCnt: 0, Action:action}, 
+            {emailContact:"a1v1@rr.com", emailCnt: 23333, Action:action },
+            {emailContact:"3av1@rr.com", emailCnt: 12, Action:action },
+            {emailContact:"tesfft1@yafffn.com", emailCnt: 54, Action:action}, 
+            {emailContact:"ff@rr.com", emailCnt: 909, Action:action },
+            {emailContact:"zz1@rr.com", emailCnt: 321, Action:action }];
 
 var orderData = function(data){
-for (const [key, val] of Object.entries(data)) {
-    var row = table.insertRow(rowCnt++)
+var i = 0;
+for( i =0; i< data.length; i++){
+    var row = table.insertRow(i+1)
     cellCnt = 0;
-for (const [key, value] of Object.entries(val)) {
+for (const [key, value] of Object.entries(data[i])) {
     var cell = row.insertCell(cellCnt++);
     cell.innerHTML = value;
   }
 }
 }
+
 orderData(data);
-var sortAlphUp =  function() {
-    rowCnt = 1;
+
+function compareAsc( a, b ) {
+    if ( a.emailCnt < b.emailCnt ){
+      return -1;
+    }
+    if ( a.emailCnt > b.emailCnt ){
+      return 1;
+    }
+    return 0;
+  }
+
+function compareDesc( a, b ) {
+if ( a.emailCnt < b.emailCnt ){
+  return 1;
+}
+if ( a.emailCnt > b.emailCnt ){
+  return -1;
+}
+return 0;
+}
+
+function compareNumAsc( a, b ) {
+  if ( a.emailCnt < b.emailCnt ){
+    return -1;
+  }
+  if ( a.emailCnt > b.emailCnt ){
+    return 1;
+  }
+  return 0;
+}
+
+function compareNumDesc( a, b ) {
+  if ( a.emailCnt < b.emailCnt ){
+    return 1;
+  }
+  if ( a.emailCnt > b.emailCnt ){
+    return -1;
+  }
+  return 0;
+}
+
+function compareStrAsc( a, b ) {
+  if ( a.emailContact < b.emailContact ){
+    return -1;
+  }
+  if ( a.emailContact > b.emailContact ){
+    return 1;
+  }
+  return 0;
+}
+
+function compareStrDesc( a, b ) {
+  if ( a.emailContact < b.emailContact ){
+    return 1;
+  }
+  if ( a.emailContact > b.emailContact ){
+    return -1;
+  }
+  return 0;
+}
+
+var sortAlphUp = function(){
+    data.sort(compareStrAsc);
     cellCnt = 0;
-    var sortedData =  Object.keys(data).sort(function(a,b){
-        return data[a].emailCnt - data[b].emailCnt
-    });
-    orderData(sortedData);
+   for(j = data.length; j>0; j--)
+    table.deleteRow(j);
+    orderData(data);
+}
+
+var sortAlphDown = function(){
+  data.sort(compareStrDesc);
+  cellCnt = 0;
+ for(j = data.length; j>0; j--)
+  table.deleteRow(j);
+  orderData(data);
+}
+
+var sortNumUp = function(){
+  data.sort(compareNumAsc);
+  cellCnt = 0;
+ for(j = data.length; j>0; j--)
+  table.deleteRow(j);
+  orderData(data);
+}
+
+var sortNumDown = function(){
+data.sort(compareNumDesc);
+cellCnt = 0;
+for(j = data.length; j>0; j--)
+table.deleteRow(j);
+orderData(data);
 }
