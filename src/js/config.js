570a772a0f459ba7e66fd7f25e804256c88fb8d3
Guitarhub786 <codewithdrawal@gmail.com>
2020-08-30 00:04:16 +0100

    deleted config.js

--- src/js/config.js
+++
@@ -1,3 +0,0 @@
-// This key has be regenerated and will no longer work.
-const config = { API_KEY: 'AIzaSyDAehBOQpR1SVtEvX7k5Dd30B9UOjRD3B0', CLIENT_ID: '934400141440-54p6jlrrhsb7b8vttqlsip9tdci9p2n8.apps.googleusercontent.com' }
-export { config }
