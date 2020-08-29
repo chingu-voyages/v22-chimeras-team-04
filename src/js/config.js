92353b8b644b8f488d6af005a63b6f392d0aa7f5
Guitarhub786 <codewithdrawal@gmail.com>
2020-08-29 23:48:38 +0100

    update .gitignore

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
+// This key has be regenerated and will no longer work.
 const config = { API_KEY: 'AIzaSyDAehBOQpR1SVtEvX7k5Dd30B9UOjRD3B0', CLIENT_ID: '934400141440-54p6jlrrhsb7b8vttqlsip9tdci9p2n8.apps.googleusercontent.com' }
 export { config }
