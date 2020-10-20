78f59372884c495d6ef97885486c1774a58fc43f
yanpelov <11660378+yanpelov@users.noreply.github.com>
2020-10-20 23:10:15 +0300

    Added a pop logic, so that it will appear upon click on triangle, and disappear when clicking outside of it. also added hovering styling and added handling for none selection.

--- src/js/checkboxDeletion.js
+++ src/js/checkboxDeletion.js
@@ -1,13 +1,16 @@
+import { brotliDecompressSync } from "zlib";
+
 let checkboxSelect = document.getElementById("checkBoxSelect");
 let selectAll = document.getElementById("selectAll");
 let selectNone = document.getElementById("selectNone");
 let topSendersTbl = document.getElementById("topSendersTbl");
+let tablePageBody = document.getElementById("tablePageBody");
 
 let openSelectListHandler = function(event){
     let selectOpts = document.querySelector('.selectOptsPopUp');
     selectOpts.style.display = "inline-block";
-
-
+    event.stopPropagation();
+    tablePageBody.addEventListener('click', hideSelectPopup, {once:true});
 }
 
 let selectAllHandler = function(event){
@@ -19,11 +22,36 @@ let selectAllHandler = function(event){
         inputCheck = topSendersTbl.rows[i].cells[0].children[0];
         inputCheck.setAttribute('checked','');
     }
+
+    event.stopPropagation();
+
 }
 
-checkboxSelect.addEventListener('click', openSelectListHandler, { once: true });
-selectAll.addEventListener('click', selectAllHandler, { once: true });
-selectAll.addEventListener('click', selectNoneHandler, { once: true });
+
+let selectNoneHandler = function(event){
+    let length = topSendersTbl.rows.length;
+    let row = null;
+    let inputCheck = null;
+    for(let i = 0; i < length ; i++)
+    {
+        inputCheck = topSendersTbl.rows[i].cells[0].children[0];
+        inputCheck.removeAttribute('checked');
+    }
+
+    event.stopPropagation();
+
+}
+
+let hideSelectPopup = function(event){
+    let selectOpts = document.querySelector('.selectOptsPopUp');
+    selectOpts.style.display = "none";
+}
+
+
+checkboxSelect.addEventListener('click', openSelectListHandler);
+selectAll.addEventListener('click', selectAllHandler);
+selectNone.addEventListener('click', selectNoneHandler);
+
 
 
 
